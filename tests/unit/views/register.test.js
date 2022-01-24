acdbdfa3506fbab8c457d17e01bdd34f8a291760
Adrien Lbt <adrienlambert725@msn.com>
2022-01-24 16:57:05 +0100

    --- Installation vue-jest pour transformer les fichier vue en fichier js que jest peut parser --- Création du fichier de test pour la view Register

---
+++ tests/unit/views/register.test.js
@@ -0,0 +1,14 @@
+import { shallowMount } from '@vue/test-utils'
+import Register from '../../../src/views/Register.vue'
+
+describe('Register user', () => {
+
+    it(`Should redirect to Home view`, () => {
+        expect(1+1).toBe(2);
+    });
+
+    // it(`Should display error alert`, () => {
+
+    // });
+
+});
