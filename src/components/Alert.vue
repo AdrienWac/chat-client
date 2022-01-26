98341a5d878c8a54ba08c15dc922ffec02e1c8dd
Adrien Lbt <adrienlambert725@msn.com>
2022-01-26 12:38:29 +0100

    --- Implémentation test display error message in component alert. --- Implémentation component alert

---
+++ src/components/Alert.vue
@@ -0,0 +1,39 @@
+<template>
+    <div v-show="visible" :class="`alert alert-${type}`" role="alert">
+        <span>{{ messsage }}</span>
+        <button type="close" @click="close">Close</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Alert',
+    props: {
+        type: {
+            default() {
+                return null;
+            },
+            validator: function(value) {
+                return ['success', 'danger', 'warning', 'info'].indexOf(value) !== -1;
+            }
+        },
+        message: {
+            type: String,
+            required: true,
+            default() {
+                return '';
+            }
+        }
+    },
+    computed: {
+        visible() {
+            return this.type !== null && this.message.trim().length > 0; 
+        }
+    },
+    methods: {
+        close() {
+
+        }
+    }
+}
+</script>
---
+++ tests/unit/components/alert.test.js
@@ -0,0 +1,44 @@
+/**
+ * @jest-environment jsdom
+ */
+import { shallowMount } from '@vue/test-utils'
+import Alert from '../../../src/components/Alert.vue'
+
+describe(`Alert component`, () => {
+
+    it(`Should display an error message`, async () => {
+
+        const wrapper = shallowMount(Alert, {
+            props: {
+                type: null,
+                message: ''
+            }
+        });
+
+        // Tester que le rendu html ne contient pas le bloc avec classe alert
+        expect(wrapper.get('.alert').isVisible()).toBe(false);
+
+        // Mise à jour des props type et message
+        await wrapper.setProps({ type: 'danger', message: 'Ceci est un message d\'erreur'});
+
+        // Tester que le rendu html du component contient bien le message
+        expect(wrapper.get('.alert').isVisible()).toBe(true);
+
+        // Tester que le bloc alert a bien la classe en lien avec le type d'error
+        expect(wrapper.find('.alert-danger').exists()).toBe(true);
+
+    });
+
+    // it(`Should close alert`, async() => {
+        
+    //     // ShallowMount avec props type et message
+
+    //     // Test que le rendu html contient bien la balise .alert et que la computed visible est à true
+
+    //     // Find button close et trigger click
+
+    //     // Test que la computed visible est bien passée à false et que le rendu html ne contient plus la balise .alert
+
+    // });
+
+});
