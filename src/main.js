f891d7e36fc136d13d5eba6f4ba96a782b2a58ad
Adrien Lbt <adrienlambert725@msn.com>
2022-02-02 11:16:14 +0100

    --- Fix & log env variables

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
+console.log('process.env :', process.env);
+console.log('process.env.VUE_APP_URL_API :', process.env.VUE_APP_URL_API);
+console.log('process.env.NODE_ENV :', process.env.NODE_ENV);
+
 createApp(App).use(store).use(router).mount('#app')
