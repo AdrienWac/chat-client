089e69cd259db96767d32560c42fb26c354a62a5
Adrien Lbt <adrienlambert725@msn.com>
2022-02-03 12:02:01 +0100

    --- Affichage des username des utilisateurs connectés dans l'aside --- Traitement d'un nouvel utilisateur connecté dans l'aside

---
+++ src/components/chat/Aside.vue
@@ -0,0 +1,62 @@
+<template>
+    <aside>
+        <ul>
+            <li v-for="user in users"> {{user.username}}</li>
+        </ul>
+    </aside>
+</template>
+
+<script>
+import {onMounted, ref, reactive, watch} from 'vue'
+import Socket from '../../socket'
+
+export default {
+    name: 'Aside',
+    props: {
+        room: {
+            default() {
+                return '/';
+            }
+        },
+    },
+    setup(props) {
+        
+        let users = ref([]);
+
+        const generateListUsers = (arrayUsers) => {
+
+            arrayUsers.forEach(userData => {
+                userData.sel = userData.userId === Socket.id;
+            });
+
+            return arrayUsers.sort((a, b) => {
+                // a sera placé avant b
+                if (a.self) return -1;
+                // b sera placé avant a
+                if (b.self) return 1;
+                // Si b est supérieur a a, b sera placé avant a
+                if (a.username < b.username) return 1;
+                // Si a est supérieur a b, a sera placé avant b
+                if (a.username > b.username) return -1;
+                // Sinon a & b sont égaux on retourne 0
+                return 0;
+            });
+        }
+
+
+        onMounted(() => {
+            Socket.on('users', (usersOnSocket) => {
+                users.value = generateListUsers(usersOnSocket);
+            });
+        });
+
+        Socket.on('user connected', (userInformation) => {
+            users.value.push(userInformation);
+        });
+
+        return {users};
+  },
+}
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <Aside :room="'sqqs'"/>
+    
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <form @submit.prevent="createUsername">
@@ -14,7 +16,11 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-console.log(sessionStorage);
+import Socket from '../socket'
+import {onMounted} from 'vue'
+import Aside from '../components/chat/Aside.vue'
+
+
 export default {
   name: 'Home',
   data: () => ({
@@ -22,8 +28,29 @@ export default {
       username: null
     }
   }),
+  setup() {
+    
+    let user = JSON.parse(sessionStorage.getItem('user'));
+    console.log(user.username);
+
+    const initSocket = () => { Socket.auth = user; Socket.connect();console.log('init Socket', Socket.id) }
+    
+    onMounted(() => {
+      initSocket();
+    });
+    
+    Socket.on("connect_error", (err) => {
+      console.log('errr', err);
+      if (err.message === "invalid username") {
+        this.usernameAlreadySelected = false;
+      }
+    });
+
+    return { name };
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    Aside
   }
 }
 </script>
