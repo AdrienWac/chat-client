16b4a40561b283b63f3baa96608f55b6781a7fcf
Adrien Lbt <adrienlambert725@msn.com>
2022-02-04 12:21:25 +0100

    --- Intégration de l'interface de chat

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,115 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <Aside :users="listUsers" :room="'sqqs'" @selectionUser="selectUser"/>
-    Utilisateur sélectionné : {{selectedUser.userId}}
-    <Form @sendMessage="sendMessage" v-if="selectedUser.userId"/>
-    <div v-if="selectedUser.userId">
-      <p>Discussion avec {{selectedUser.username}}</p>
-      <ul>
-        <li v-for="message in selectedUser.messages">
 
-            <span>{{message.fromSelf ? "Moi" : selectedUser.username}}</span>
-            <span>{{message.content}}</span>
+    <main>
 
-        </li>
-      </ul>
-    </div>
+      <div class="forum">
+
+        <div class="forum__header">
+          <!-- <div class="user header__user" v-if="selectedUser.userId">{{selectedUser.username}}</div> -->
+          <div data-state="offline" class="user header__user">Lorem Ipsum is simply dummy text of</div>
+        </div>
+        
+        <div class="forum__list">
+          <ul>
+
+            <li class="message__card">
+              
+              <div class="message__aside">
+                <img alt="Vue logo" src="../assets/logo.png">
+              </div>
+
+              <div class="message__main">
+
+                <div class="message__main-header"> 
+                  <span data-state="offline" class="message__sender user"> AdrienLbt </span> 
+                  <span class="message__time"> 04/02/2022 - 14h55 </span> 
+                </div>
+
+                <div class="message__content"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum </div>
+
+              </div>
+        
+            </li>
+
+            <li class="message__card">
+              
+              <div class="message__aside">
+                <img alt="Vue logo" src="../assets/logo.png">
+              </div>
+
+              <div class="message__main">
+
+                <div class="message__main-header"> 
+                  <span data-state="offline" class="message__sender user"> AdrienLbt </span> 
+                  <span class="message__time"> 04/02/2022 - 14h55 </span> 
+                </div>
+
+                <div class="message__content"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum </div>
+
+              </div>
+        
+            </li>
+
+            <li class="message__card">
+              
+              <div class="message__aside">
+                <img alt="Vue logo" src="../assets/logo.png">
+              </div>
+
+              <div class="message__main">
+
+                <div class="message__main-header"> 
+                  <span data-state="offline" class="message__sender user"> AdrienLbt </span> 
+                  <span class="message__time"> 04/02/2022 - 14h55 </span> 
+                </div>
+
+                <div class="message__content"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum </div>
+
+              </div>
+        
+            </li>
+
+            <li class="message__card">
+              
+              <div class="message__aside">
+                <img alt="Vue logo" src="../assets/logo.png">
+              </div>
+
+              <div class="message__main">
+
+                <div class="message__main-header"> 
+                  <span data-state="offline" class="message__sender user"> AdrienLbt </span> 
+                  <span class="message__time"> 04/02/2022 - 14h55 </span> 
+                </div>
+
+                <div class="message__content"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum </div>
+
+              </div>
+        
+            </li>
+            
+            <!-- <li class="list__item" v-for="message in selectedUser.messages">
+                <span>{{message.fromSelf ? "Moi" : selectedUser.username}}</span>
+                <span>{{message.content}}</span>
+            </li> -->
+          </ul>
+        </div>
+        
+        <div class="forum__footer">
+          <span class="user__action--write">Username est en train d'écrire</span>
+        </div>
+
+      </div>
+
+      <Form @sendMessage="sendMessage"/>
+      <!-- <Form @sendMessage="sendMessage" v-if="selectedUser.userId"/> -->
+
+    </main>
     
   </div>
+
 </template>
 
 <script>
@@ -35,12 +128,6 @@ export default {
     },
     
   }),
-  // methods: {
-  //     selectUser(user) {
-  //         console.log('select user', user);
-  //         // this.$emit('selectionUser', user);
-  //     }
-  // },
   setup() {
 
     let listUsers = ref([]);
@@ -143,3 +230,100 @@ export default {
   }
 }
 </script>
+<style scoped lang="scss">
+
+.user:before {
+  content: '';
+  height: 8px;
+  width: 8px;
+  border-radius: 50%;
+  display: inline-block;
+  margin-right: 6px;
+}
+
+.user[data-state="online"]:before {background-color: greenyellow;}
+.user[data-state="offline"]::before {background-color: black;}
+
+.home {
+
+  font-family: $oswald;
+  font-weight: lighter;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: grid;
+  grid-template-columns: 30% 70%;
+  grid-template-rows: 100%;
+
+  aside{background: map-get($colors, primary);}
+
+  main{
+    
+    background: map-get($colors, second);
+    display: grid;
+    grid-template-rows: 80% 20%;
+    
+    .forum {
+
+      display: grid;
+      grid-template-rows: 10% 80% 10%;
+
+      .forum__header {
+
+        min-width: 320px;
+        max-width: 100%;
+        position: relative;
+        padding: 10px 0 10px 5px;
+        border-bottom: 1px solid #eef0f2;
+
+        
+
+      }
+
+      .forum__list {
+        
+        overflow-y: scroll;
+
+        .message__card {
+
+          padding: 15px 5px;
+          display: grid;
+          grid-template-columns: 1fr 11fr;
+
+          .message__aside {
+            
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: start;
+
+            img {max-width:32px; max-height: 32px;}
+
+          }
+
+          .message__main-header {
+            .message__sender{ font-weight: normal;font-size: 1.1rem;}
+            .message__time{ font-size: 0.7rem; color: lighten(map-get($fontColors, primary), 20%);}
+          }
+
+          .message__content {
+            line-height: 1.2rem;
+            padding: 5px 0;
+            font-size: 1.05rem;
+            text-align: justify;
+          }
+
+        }
+
+      }
+
+    }
+
+  }
+
+
+
+}
+</style>
