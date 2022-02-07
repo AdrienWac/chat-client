1115bf7660cc77af05df23bf94e2fcc83fded33a
Adrien Lbt <adrienlambert725@msn.com>
2022-02-07 22:16:03 +0100

    --- Fix trouble connexion

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client"
 import config from './config'
 
-const socket = io(config.APP_URL, {autoConnect: false});
+console.log('config.API_URL', config.API_URL);
+const socket = io(config.API_URL, {autoConnect: false});
 
 socket.onAny((event, ...args) => {
     console.log(`Catch-all Listener event: ${event} args : ${args}`);
