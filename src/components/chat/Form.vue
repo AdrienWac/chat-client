4396b91c9b957f396c76dcefd15efcd8a8bd687a
Adrien Lbt <adrienlambert725@msn.com>
2022-02-20 16:12:20 +0100

    --- Switch key event to watcher on content variable to fire typing event to server.

--- src/components/chat/Form.vue
+++ src/components/chat/Form.vue
@@ -3,7 +3,7 @@
   <div>
     
     <form @submit.prevent="onSubmit" class="form">
-      <textarea v-model="content" placeholder="Your message..." class="input" @keyup.ctrl.exact="onSubmit" @keyup="isTyping"/>
+      <textarea v-model="content" placeholder="Your message..." class="input" @keyup.ctrl.exact="onSubmit"/>
       <button :disabled="!isValid" class="send-button">
         <PaperPlane :stroke="{color: 'transparent', width:3}" :fill="'#fff'" height="24" width="24" />
       </button>
@@ -15,7 +15,7 @@
 
 <script>
 
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 import {useStore} from 'vuex'
 import PaperPlane from '../svg/PaperPlane.vue'
 import Socket from '../../socket'
@@ -39,12 +39,17 @@ export default {
 
         const user = JSON.parse(localStorage.getItem('user'));
 
-        const isTyping = () => {
 
-          Socket.emit('user typing', { content: content.value, userSender: user, userReceiver:  store.getters['chat/selectedUser']});
-         
-          
-        }
+        watch(content, (newValue, oldValue) => {
+          // Si l'ancienne value est vide et que la nouvelle est pleine
+          if (!oldValue.length && newValue.length) {
+            Socket.emit('user typing', { content: content.value, userSender: user, userReceiver:  store.getters['chat/selectedUser']});
+          } else if(!newValue.length && oldValue.length) {
+            Socket.emit('user typing', { content: content.value, userSender: user, userReceiver:  store.getters['chat/selectedUser']});
+          } else {
+            console.log('Still typing');
+          }
+        });
 
          // TODO a refacto
           Socket.on('end typing', ({user: userWhoTyping}) => store.dispatch('chat/detectTypingMessage', {user: userWhoTyping, state: false}));
